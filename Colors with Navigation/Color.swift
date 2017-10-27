ab027b6f7e0855badd864e319967c31f15ef6b4c
AndrewStoll <aws52b@mail.missouri.edu>
2017-10-27 10:46:21 -0500

    Added background color

---
+++ Colors with Navigation/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  Colors with Navigation
+//
+//  Created by Andrew W Stoll on 10/27/17.
+//  Copyright © 2017 Andrew W Stoll. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    let name:String
+    let uiColor:UIColor
+}
